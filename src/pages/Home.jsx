8ebd04904766ef90d0fa87313f9bc23726192665
hagay kohelet <hagay2506@gmail.com>
2026-03-01 17:58:08 +0200

    not finish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Nav from '../components/Nav'
-
+import data from '../../public/db/products.json'
+import '../style/Home.css'
 function Home() {
   return (
     <div>
       <Nav />
+      <main>
+        <p>EXERCISE IDEA</p>
+        <h2>Use global state to keep the cart in sync everywhere</h2>
+      </main>
     </div>
   )
 }
